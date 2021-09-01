131cd7eec87fcf1b6dee81f25ed8fc24831eccbb
laurag <lgaffey01@qub.ac.uk>
2021-09-01 16:38:45 +0100

    first commit

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -8,6 +8,12 @@ public class Calculator {
 	public int sub(int a, int b) {
 		return a-b;
 	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello");
+	} 
 	
 }
 
